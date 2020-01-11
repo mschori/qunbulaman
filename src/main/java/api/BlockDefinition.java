82a35ae61e5dfbe2ca6597fc73756ceb7121415b
mschori <michael.schori@gmx.ch>
2020-01-11 11:57:08 +0100

    working...

---
+++ src/main/java/api/BlockDefinition.java
@@ -0,0 +1,14 @@
+package api;
+
+public class BlockDefinition {
+
+    Integer emptyBlock = 50;
+    Integer destroyableBlock = 51;
+    Integer undestroyableBlock = 52;
+    Integer bombBlock = 53;
+    Integer playerBlock1 = 54;
+    Integer getPlayerBlock2 = 55;
+    Integer getPlayerBlock3 = 56;
+    Integer getPlayerBlock4 = 57;
+
+}
